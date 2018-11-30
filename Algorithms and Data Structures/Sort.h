93fefd8ac95b698e0ef11df3f67dacf990de01aa
RooTender <lewandowski.hubert486@gmail.com>
2018-12-01 00:12:15 +0100

    Update Sort.h

--- Algorithms and Data Structures/Sort.h
+++ Algorithms and Data Structures/Sort.h
@@ -4,11 +4,12 @@ class Sort
 private:
 	int *arr, arrLength;
 	void swap(int &a, int &b);
+	void merge(int &firstIndex, int &middleIndex, int &lastIndex); // middleIndex is between two separated arrays
 public:
 	Sort(int *arr, int &arrLength);
 	void selectionSort();
 	void bubbleSort();
 	void insertionSort();
+	void mergeSort(int firstIndex = 0, int lastIndex = -1);	// -1 means undefined! -> First initialization
 	~Sort();
 };
-
